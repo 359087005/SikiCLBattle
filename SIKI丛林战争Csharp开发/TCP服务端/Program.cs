c1866940a9fd965dcd17d2902a483b08739d840c
DKS-20160923UBY\Administrator <359087005@qq.com>
2018-07-30 09:24:21 +0800

    初期所有文件上传

--- SIKI丛林战争Csharp开发/TCP服务端/Program.cs
+++ SIKI丛林战争Csharp开发/TCP服务端/Program.cs
@@ -26,22 +26,48 @@ namespace TCP服务端
             serverSocket.Bind(point);
             serverSocket.Listen(10); //socket放置与侦听状态 设置监听最大值
 
-            Socket clientSocket = serverSocket.Accept(); //接收一个客户端连接
+            //Socket clientSocket = serverSocket.Accept(); //接收一个客户端连接
+            serverSocket.BeginAccept(AcceptCallBack, serverSocket);
+
+        }
+        static void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket serverSocket = ar.AsyncState as Socket;
+            Socket clientSocket = serverSocket.EndAccept(ar);
+
             string msg = "Hello Client!this is Server";
             clientSocket.Send(Encoding.UTF8.GetBytes(msg));
 
             clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);
 
+            serverSocket.BeginAccept(AcceptCallBack, serverSocket);
         }
-        static byte[] dataBuffer = new byte[1024];
-       static void ReceiveCallBack(IAsyncResult ar)
-        {
-            Socket clientSocket = ar.AsyncState as Socket;
-            int count = clientSocket.EndReceive(ar);
-            string msg = Encoding.UTF8.GetString(dataBuffer, 0, count);
-            Console.WriteLine("从客户端接收的数据:" + msg);
-            clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);
 
+        static byte[] dataBuffer = new byte[1024];
+        static void ReceiveCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = null;
+            try
+            {
+                clientSocket = ar.AsyncState as Socket;
+                int count = clientSocket.EndReceive(ar);
+                if (count == 0)
+                {
+                    clientSocket.Close();
+                    return;
+                }
+                string msg = Encoding.UTF8.GetString(dataBuffer, 0, count);
+                Console.WriteLine("从客户端接收的数据:" + msg);
+                clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                if (clientSocket != null)
+                {
+                    clientSocket.Close();
+                }
+            }
         }
     }
 
